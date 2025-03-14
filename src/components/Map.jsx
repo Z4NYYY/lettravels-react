416b67a81483aba3d5aea2eacefcbee6ab5ea0c4
Z4NYYY <pichayathumma@gmail.com>
2025-03-14 08:53:47 +0700

    Updated Map component to always show the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -13,8 +13,6 @@ const center = {
 
 export default function Map() {
   return (
-    <div className="bg-gray-200 py-12">
-      <h2 className="text-center text-3xl font-bold mb-8">Explore the Map</h2>
       <div className="mt-8 flex justify-center">
         <LoadScript googleMapsApiKey="AIzaSyBHwAddk9t61U-k54ZdlqN96fzGsPtbjc0">
           <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
@@ -22,7 +20,6 @@ export default function Map() {
           </GoogleMap>
         </LoadScript>
       </div>
-    </div>
   );
 }
 
