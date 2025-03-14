3f75462574f4a7afcfc0b98802823c079718792e
Z4NYYY <pichayathumma@gmail.com>
2025-03-14 08:47:58 +0700

    Updated Map component and change api

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -12,36 +12,17 @@ const center = {
 };
 
 export default function Map() {
-  const [showMap, setShowMap] = useState(false);
-
-  const toggleMap = () => {
-    setShowMap((prev) => !prev);
-  };
-
   return (
     <div className="bg-gray-200 py-12">
       <h2 className="text-center text-3xl font-bold mb-8">Explore the Map</h2>
-      <div className="text-center">
-        <button
-          onClick={toggleMap}
-          className="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-600"
-        >
-          {showMap ? "Hide Map" : "Show Map"}
-        </button>
+      <div className="mt-8 flex justify-center">
+        <LoadScript googleMapsApiKey="AIzaSyBHwAddk9t61U-k54ZdlqN96fzGsPtbjc0">
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+            <Marker position={center} />
+          </GoogleMap>
+        </LoadScript>
       </div>
-      {showMap && (
-        <div className="mt-8">
-          <LoadScript googleMapsApiKey="AIzaSyBHwAddk9t61U-k54ZdlqN96fzGsPtbjc0">
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              center={center}
-              zoom={10}
-            >
-              <Marker position={center} />
-            </GoogleMap>
-          </LoadScript>
-        </div>
-      )}
     </div>
   );
 }
+
